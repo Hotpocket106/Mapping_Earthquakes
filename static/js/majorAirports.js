a058e1365196b1af583bbd997b19626741787d3e
Hotpocket106 <panwei106521@gmail.com>
2022-12-15 13:09:27 -0600

    work

--- static/js/majorAirports.js
+++ static/js/majorAirports.js
@@ -1,1 +1,16 @@
 let map = L.map('mapid').setView([30, 30], 2);
+
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+dark.addTo(map);
+
+let airportData = "https://raw.githubusercontent.com/Hotpocket106/Mapping_Earthquakes/main/static/js/majorAirports.json";
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+});
